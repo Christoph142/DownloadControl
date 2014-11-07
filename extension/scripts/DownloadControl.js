41bf180ddd64cef211bb81e6127db9d677eac415
Christoph142 <christoph142@gmx.com>
2014-11-07 19:57:59 +0100

    fix notification console error

--- extension/scripts/DownloadControl.js
+++ extension/scripts/DownloadControl.js
@@ -346,8 +346,9 @@ function removeDownloadFromList(change){
 
 // show desktop notification of download completion or stop
 function notifyDownloadState(change){
+	if( !change.state ) return;
 	// only if download is complete or interrupted and according notification option is turned on
-	if( ( change.state.current === "complete" && w.notifyDone === "1" ) || ( change.state.current === "interrupted" && w.notifyFail === "1" && change.error.indexOf("USER") !== 0 ) )
+	else if( ( change.state.current === "complete" && w.notifyDone === "1" ) || ( change.state.current === "interrupted" && w.notifyFail === "1" && change.error.indexOf("USER") !== 0 ) )
 		chrome.downloads.search({"id": change.id}, function(ds){
 			var filename = ds[0].filename.substring(ds[0].filename.lastIndexOf("\\") + 1);
 			if( ds[0].state === "complete" && w.notifyDone === "1" )
